ab4e6577267765646d3ddf8d2a2535c70a53f9f8
Nicolas DI PRIMA <nicolas@di-prima.fr>
2014-03-05 07:15:38 +0000

    minor update the defaultKKEntity

--- System/Keeper/Types.hs
+++ System/Keeper/Types.hs
@@ -1,38 +1,46 @@
-module System.Keeper.Types where
+module System.Keeper.Types
+    ( KKEntity(..)
+    , defaultKKEntity
+    , UserName(..)
+    ) where
 
 import Data.ByteString as BS (ByteString, empty)
 import Data.Byteable
 
+-- | Keeper Key Entity:
+-- it represents an AUTHORIZED_KEY. It implements the FORMAT given in the
+-- SSHD_CONFIG manual.
 data KKEntity = KKEntity
-    { name              :: ByteString
-    , pubKey            :: ByteString
-    , certAuthority     :: Maybe Bool
-    , command           :: Maybe ByteString
-    , environment       :: Maybe ByteString
-    , from              :: Maybe ByteString
-    , noAgentForwarding :: Maybe Bool
-    , noPortForwarding  :: Maybe Bool
-    , noPTY             :: Maybe Bool
-    , noUserRC          :: Maybe Bool
-    , noX11Forwarding   :: Maybe Bool
-    , permitOpen        :: Maybe ByteString
-    , principals        :: Maybe ByteString
-    , tunnel            :: Maybe ByteString
+    { name              :: ByteString       -- ^ an associated name
+    , pubKey            :: ByteString       -- ^ the public KEY
+    , certAuthority     :: Maybe Bool       -- ^ cert-authority
+    , command           :: Maybe ByteString -- ^ command="command"
+    , environment       :: Maybe ByteString -- ^ environment="NAME=value"
+    , from              :: Maybe ByteString -- ^ from="pattern-list"
+    , noAgentForwarding :: Maybe Bool       -- ^ no-agent-forwarding
+    , noPortForwarding  :: Maybe Bool       -- ^ no-port-forwarding
+    , noPTY             :: Maybe Bool       -- ^ no-pty
+    , noUserRC          :: Maybe Bool       -- ^ no-user-rc
+    , noX11Forwarding   :: Maybe Bool       -- ^ no-X11-forwarding
+    , permitOpen        :: Maybe ByteString -- ^ permitopen="host:port"
+    , principals        :: Maybe ByteString -- ^ principals="principals"
+    , tunnel            :: Maybe ByteString -- ^ tunnel="n"
     } deriving (Show, Read, Eq)
 
+-- | returns a default KKEntity
 defaultKKEntity :: KKEntity
 defaultKKEntity = KKEntity
     { name              = BS.empty
     , pubKey            = BS.empty
-    , certAuthority     = Just False
+    , certAuthority     = Nothing
     , command           = Nothing
     , environment       = Nothing
     , from              = Nothing
-    , noAgentForwarding = Just False
-    , noPortForwarding  = Just False
-    , noPTY             = Just False
-    , noUserRC          = Just False
-    , noX11Forwarding   = Just False
+    , noAgentForwarding = Nothing
+    , noPortForwarding  = Nothing
+    , noPTY             = Nothing
+    , noUserRC          = Nothing
+    , noX11Forwarding   = Nothing
     , permitOpen        = Nothing
     , principals        = Nothing
     , tunnel            = Nothing
